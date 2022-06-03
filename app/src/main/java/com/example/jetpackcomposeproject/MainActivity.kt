cd1ded3987a0342677f9ccd2e4b3c8b051716668
Romilkumar Patel <patelromil2800@gmail.com>
2022-06-03 12:00:52 -0300

    Text Customization

--- app/src/main/java/com/example/jetpackcomposeproject/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeproject/MainActivity.kt
@@ -14,6 +14,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,6 +47,16 @@ class MainActivity : ComponentActivity() {
 //                    ) {
 //                        CustomItem(weight = 3f, color = MaterialTheme.colors.secondary)
 //                        CustomItem(weight = 1f)
+//                    }
+
+
+//                    Text Customization
+//                    Column(modifier = Modifier.fillMaxSize()) {
+//                        Text(
+//                            text = stringResource(id = R.string.app_name), modifier = Modifier
+//                                .background(MaterialTheme.colors.primary)
+//                                .padding(16.dp)
+//                        )
 //                    }
                 }
             }
@@ -68,17 +87,84 @@ fun DefaultPreview() {
 //            CustomItem(weight = 1f)
 //        }
 
-        Greeting()
+//        Greeting()
+
+
+//        Text Customization
+        Column(modifier = Modifier.fillMaxSize()) {
+//            Text(
+//                text = stringResource(id = R.string.app_name), modifier = Modifier
+//                    .background(MaterialTheme.colors.primary)
+//                    .padding(16.dp)
+//                    .width(200.dp),
+//                color = Color.White,
+//                fontSize = MaterialTheme.typography.h6.fontSize,
+//                fontStyle = FontStyle.Italic,
+//                fontWeight = FontWeight.Bold,
+//                textAlign = TextAlign.Center,
+//            )
+
+            CustomText3()
+        }
+
+
     }
 }
 
 @Composable
 fun Greeting() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopCenter) {
-        Box(modifier = Modifier
-            .background(Color.Blue), contentAlignment = Alignment.Center){
-            Box(modifier = Modifier.height(50.dp).width(50.dp).background(Color.Green))
-            Text(text = "I love Android!", fontSize = 40.sp)
-        }
-    }
+
+//    Box
+//    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopCenter) {
+//        Box(modifier = Modifier
+//            .background(Color.Blue), contentAlignment = Alignment.Center){
+//            Box(modifier = Modifier.height(50.dp).width(50.dp).background(Color.Green))
+//            Text(text = "I love Android!", fontSize = 40.sp)
+//        }
+//    }
+
+}
+
+
+@Composable
+fun CustomText() {
+    Text(
+        text = stringResource(id = R.string.app_name),
+        modifier = Modifier
+            .background(MaterialTheme.colors.primary)
+            .padding(16.dp)
+            .width(200.dp),
+        color = Color.White,
+        fontSize = MaterialTheme.typography.h6.fontSize,
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center,
+    )
+}
+
+@Composable
+fun CustomText2() {
+    Text(
+        buildAnnotatedString {
+            withStyle(style = ParagraphStyle(textAlign = TextAlign.Center)){
+                withStyle(
+                    style = SpanStyle(
+                        color = MaterialTheme.colors.primary,
+                        fontSize = 60.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                ) {
+                    append("A")
+                }
+                append("B")
+                append("C")
+                append("D")
+            }
+        }, modifier = Modifier.width(200.dp)
+    )
+}
+
+@Composable
+fun CustomText3(){
+    Text(text = "Hello World".repeat(20), maxLines = 2, overflow = TextOverflow.Ellipsis)
 }
